3c1068242ca674a115b045a89ce7a939b64ee9e7
nguoianphu <nguoianphu@gmail.com>
2014-01-14 14:34:01 +0700

    commit new files

---
+++ src/main/java/com/java/bin/DummyDNSThread.java
@@ -0,0 +1,16 @@
+package com.java.bin;
+
+import java.io.IOException;
+
+public class DummyDNSThread implements Runnable {
+
+	public DummyDNSThread() throws IOException {
+		super();
+	}
+
+	@Override
+	public void run() {
+
+	}
+
+}
---
+++ src/test/java/com/java/bin/DummyDNSThreadTest.java
@@ -0,0 +1,28 @@
+package com.java.bin;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DummyDNSThreadTest {
+
+	// static Runnable run = new Runnable() {
+	//
+	// @Override
+	// public void run() {
+	// int continueRun = 0;
+	// while(true){
+	// System.out.println(continueRun++);
+	// }
+	// }
+	// };
+
+//	@Test
+//	public void testDummyDNSThread() throws Exception {
+//
+//		DummyDNSThread dummyDNSThread = new DummyDNSThread();
+//		Thread t = new Thread(dummyDNSThread);
+//		t.start();
+//		assertEquals("RUNNABLE", dummyDNSThread.toString());
+//	}
+
+}
